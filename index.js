b473790f8fb27a47aa92af16f5a3c36d8f0a474d
Sam Arshad <jsb211@txstate.edu>
2025-05-14 16:55:14 -0500

    Display and calculation functions of calculator working.

---
+++ index.js
@@ -0,0 +1,26 @@
+// Calculator Program
+
+const display = document.getElementById('display');
+
+//To Clear The Display
+function clearDisplay(){
+    display.value = "";
+}
+
+
+//To Show Values On The Display
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+//To Calculate Values
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
